f2dbaf7746568a30fceac0efe2d487e7128194e8
ruochanpan <rpan99@uchicago.edu>
2022-03-31 11:33:20 -0500

    Update temp.py
    Change

--- temp.py
+++ temp.py
@@ -1,2 +1,4 @@
 # Introduction
-print("hello world!")
+TempretureF = float(input("enter tempreture in F: "))
+TempretureC = (TempretureF - 32) * 5 / 9
+print(str(TempretureF) + " Fahrenheit " + "in celcius is " + str(TempretureC))
