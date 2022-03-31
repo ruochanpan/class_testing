1d58809f261895e8f663ff24da02a8663e28bff9
ruochanpan <rpan99@uchicago.edu>
2022-03-31 11:52:36 -0500

    Update temp.py
    change

--- temp.py
+++ temp.py
@@ -1,4 +1,18 @@
 # Introduction
-TempretureF = float(input("enter tempreture in F: "))
-TempretureC = (TempretureF - 32) * 5 / 9
-print(str(TempretureF) + " Fahrenheit " + "in celcius is " + str(TempretureC))
+import turtle
+t=turtle.Turtle()
+s=turtle.Screen()
+
+def drawLine(t,x1,y1,x2,y2,c):
+    t.color(c)
+    t.penup()
+    t.goto(x1,y1)
+    t.pendown()
+    t.goto(x2,y2)
+
+drawLine(t,-100,100,-50,-100,"black")
+drawLine(t,50,100,100,-100,"black")
+drawLine(t,-200,50,200,50,"black")
+drawLine(t,-200,-50,200,-50,"black")
+
+print('your hashtag is done!')
